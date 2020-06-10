c4d633404305d1026d88cb9ce1556a0ae2bc851f
wasalas <wasalas893@gmail.com>
2020-06-10 17:05:53 +0530

    categorical _data Rstudior

--- Data Preprocessing-part 01/data_preprocessing_template.R
+++ Data Preprocessing-part 01/data_preprocessing_template.R
@@ -14,3 +14,20 @@ dataset$Age=ifelse(is.na(dataset$Age),
 dataset$Salary=ifelse(is.na(dataset$Salary),
                    ave(dataset$Salary,FUN=function(x)mean(x,na.rm = TRUE)),
                    dataset$Salary)
+
+
+# Encoding categorical data Counry
+dataset$Country=factor(dataset$Country,
+                       levels = c('France','Spain','Germany'),
+                       labels = c(1,2,3))
+
+
+# Encoding categorical data Purchased
+dataset$Purchased=factor(dataset$Purchased,
+                       levels = c('No','Yes'),
+                       labels = c(0,1))
+ 
+
+
+
+
