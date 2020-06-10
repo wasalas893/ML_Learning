578788b8a4d9c0f8869ed3a1015605d9f2252d49
wasalas <wasalas893@gmail.com>
2020-06-10 16:49:55 +0530

    categorical _data spyder

--- Data Preprocessing-part 01/data_preprocessing_template.py
+++ Data Preprocessing-part 01/data_preprocessing_template.py
@@ -16,7 +16,16 @@ imputer=Imputer(missing_values='NaN',strategy='mean',axis=0)
 imputer=imputer.fit(X[:,1:3])
 X[:,1:3]=imputer.transform(X[:,1:3])
 
-#Encoding categorical data
-from sklearn.preprocessing import LabelEncoder
+#Encoding categorical data Country
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 labelencoder_X=LabelEncoder()
 X[:,0]=labelencoder_X.fit_transform(X[:,0])
+
+onehotencoder=OneHotEncoder(categorical_features=[0])
+X=onehotencoder.fit_transform(X).toarray()
+
+#Encoding purchased
+labelencoder_y=LabelEncoder()
+y=labelencoder_y.fit_transform(y)
+
+
