6753290285fb87e52f7a8a8d7ec57cbbad67dba2
wasalas <wasalas893@gmail.com>
2020-06-29 17:14:01 +0530

    tranning set test set

--- Data Preprocessing-part 01/data_preprocessing_template.R
+++ Data Preprocessing-part 01/data_preprocessing_template.R
@@ -27,7 +27,12 @@ dataset$Purchased=factor(dataset$Purchased,
                        levels = c('No','Yes'),
                        labels = c(0,1))
  
-
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split=sample.split(dataset$Purchased,SplitRatio=0.8)
+traning_set=subset(dataset,split==TRUE)
+test_set=subset(dataset,split==FALSE)
 
 
 
